d75fdcbfd856aec2dfb00f26971f1c53a7e297a5
Chambeb7 <chambeb7@mail.uc.edu>
2024-09-23 22:24:26 -0400

    “Init”

---
+++ src/part1.java
@@ -0,0 +1,21 @@
+class part1 {
+    public static void main(String[] args) {
+        String hello = "Hello World";
+        int intOperandA = 1;
+        int intOperandB = 3;
+        int sum = intOperandA + intOperandB;//
+        int Product = 7;
+        int Difference = 11;
+        int Quotient = 13;
+        int Modulo = 17;
+        double OperandA = 3.5;
+        double OperandB = 4.5;
+        double Sum = 5.5;
+        double intProduct = 6.5;
+        double intDifference = 7.5;
+        double intQuotient = 8.5;
+        double intModulo = 9.5;
+        double myLunchCost = 12.50;
+        System.out.println("The cost of my lunch is: " + myLunchCost);
+    }
+}
